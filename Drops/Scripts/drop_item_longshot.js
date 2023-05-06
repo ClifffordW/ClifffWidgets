819eeb68bed8a34a917c7d3e19aec67f4e6f9f05
misternano <cjstitt@icloud.com>
2023-05-06 10:05:32 -0400

    code cleanup and optimization

--- Drops/Scripts/drop_item_longshot.js
+++ Drops/Scripts/drop_item_longshot.js
@@ -1,94 +1,49 @@
-let root = document.documentElement;
-var myVar =
-`
-<link  rel="stylesheet" href="https://clifffordw.github.io/ClifffWidgets/Drops/Scripts/Animations/anim_rewritten.css">
+const root = document.documentElement;
+const DropReleaseDate = new Date("March 24, 2022 18:00:00").getTime();
+const drops = document.getElementsByClassName("drops")[0];
+const body =
+    `
+    <link rel="stylesheet" href="https://clifffordw.github.io/ClifffWidgets/Drops/Scripts/Animations/anim_rewritten.css">
+    <img class="bg-flair" src="https://items.kleientertainment.com/images/DST/PROFILEFLAIR_RUINS_BATAN/verylarge">
+    <img class="bg-portrait" src="https://items.kleientertainment.com/images/DST/PLAYERPORTRAIT_BG_RUINSBATAN/verylarge">
+    <img class="item-frame" src="https://clifffordw.github.io/ClifffWidgets/Drops/Dropcons/Frames/Item/default.png">
+    <img class="bg-item" src="https://items.kleientertainment.com/images/DST/RUINS_BAT_AN/verylarge">
+    <img class="partyhat" src="https://clifffordw.github.io/ClifffWidgets/Drops/Seasonal Icons/winterfeast.png">
+    <img class="wip" src="https://i.imgur.com/QqHArTg.png">
+    <img class="patreon" src="">
+    `;
 
-<img class="bg-flair" src="https://items.kleientertainment.com/images/DST/PROFILEFLAIR_RUINS_BATAN/verylarge">
-<img class="bg-portrait" src="https://items.kleientertainment.com/images/DST/PLAYERPORTRAIT_BG_RUINSBATAN/verylarge">
-<img class="item-frame" src="https://clifffordw.github.io/ClifffWidgets/Drops/Dropcons/Frames/Item/default.png">
+drops.insertAdjacentHTML("beforeEnd", body);
 
-<img class="bg-item" src="https://items.kleientertainment.com/images/DST/RUINS_BAT_AN/verylarge">
-<img class="partyhat" src="https://clifffordw.github.io/ClifffWidgets/Drops/Seasonal Icons/winterfeast.png">
-<img class="wip" src="https://i.imgur.com/QqHArTg.png">
-<img class="patreon" src="">`;
+// ANNIVERSARY — https://i.pinimg.com/originals/6e/a7/b5/6ea7b5ac3d63422d2231dbd64953d786.png
+// WINTERFEAST — https://clifffordw.github.io/ClifffWidgets/Drops/Seasonal Icons/feast.png
+// HALLOWEEN — https://clifffordw.github.io/ClifffWidgets/Drops/Seasonal Icons/halloween.png
 
+// Widget Glow
+root.style.setProperty("--glow", "#6C2192");
 
+// KLEI — #FE5200
+// TWITCH — #6C2192
 
-/*
-https://i.pinimg.com/originals/6e/a7/b5/6ea7b5ac3d63422d2231dbd64953d786.png Anniversary
-https://clifffordw.github.io/ClifffWidgets/Drops/Seasonal Icons/halloween.png Halloween
-https://clifffordw.github.io/ClifffWidgets/Drops/Seasonal Icons/feast.png Winterfeast
-*/
-
-
-
-var drops = document.getElementsByClassName('drops')[0];
-
-
-
-
-
-
-
-drops.insertAdjacentHTML('beforeEnd', myVar);
-
-
-/*Widget Glow*/
-root.style.setProperty('--glow', "#6C2192");
-
-
-/*
-
-#FE5200 Klei
-#6C2192 Twitch
-*/
-
-
-
-
-/*Party Hat for anniversary*/
-var partyhat = document.getElementsByClassName('partyhat')[0];
-partyhat.style.display = 'none';
-partyhat.style.position = 'absolute';
+/* Party Hat for anniversary */
+const partyhat = document.getElementsByClassName("partyhat")[0];
+partyhat.style.display = "none";
+partyhat.style.position = "absolute";
 partyhat.style.setProperty("transform", "rotate(35deg)");
-
-
-
-document.getElementsByClassName('wip')[0].style.display = "none"
-document.getElementsByClassName('p')[0].style.display = "none"
-
-
-document.getElementsByClassName('patreon')[0].style.display = "none"
-
-
-
-
+document.getElementsByClassName("wip")[0].style.display = "none"
+document.getElementsByClassName("p")[0].style.display = "none"
+document.getElementsByClassName("patreon")[0].style.display = "none"
 
 /*Deactivated Drops*/
-document.getElementsByClassName('bg-flair')[0].style.filter = "brightness(100%) drop-shadow(0mm 0mm 2mm rgb(0, 0, 0))";
-document.getElementsByClassName('bg-portrait')[0].style.filter = "brightness(100%) drop-shadow(0mm 0mm 2mm rgb(0, 0, 0))";
-document.getElementsByClassName('bg-item')[0].style.filter = "brightness(100%) drop-shadow(0mm 0mm 2mm rgb(0, 0, 0))";
-
-
-
-
-
-/*document.getElementsByClassName('b')[0].style.color = "#F47209"*/
-
-
-document.getElementsByClassName('wip')[0].style.position = "absolute"
-document.getElementsByClassName('wip')[0].style.left = "260px"
-document.getElementsByClassName('wip')[0].style.top = "75px"
-document.getElementsByClassName('wip')[0].style.width = "64px"
-document.getElementsByClassName('wip')[0].style.height = "auto"
-
-
-document.getElementsByClassName('wip')[0].style.filter = "drop-shadow(0mm 0mm .6mm rgb(0, 0, 0, 1))"
-
-document.getElementsByClassName('drops')[0].style.top = "100px"
-document.getElementsByClassName('drops')[0].style.position = "absolute"
-
-
-
-
-var DropReleaseDate = new Date("March 24, 2022 18:00:00").getTime();
+document.getElementsByClassName("bg-flair")[0].style.filter = "brightness(100%) drop-shadow(0mm 0mm 2mm rgb(0, 0, 0))";
+document.getElementsByClassName("bg-portrait")[0].style.filter = "brightness(100%) drop-shadow(0mm 0mm 2mm rgb(0, 0, 0))";
+document.getElementsByClassName("bg-item")[0].style.filter = "brightness(100%) drop-shadow(0mm 0mm 2mm rgb(0, 0, 0))";
+/*document.getElementsByClassName("b")[0].style.color = "#F47209"*/
+document.getElementsByClassName("wip")[0].style.position = "absolute"
+document.getElementsByClassName("wip")[0].style.left = "260px"
+document.getElementsByClassName("wip")[0].style.top = "75px"
+document.getElementsByClassName("wip")[0].style.width = "64px"
+document.getElementsByClassName("wip")[0].style.height = "auto"
+document.getElementsByClassName("wip")[0].style.filter = "drop-shadow(0mm 0mm .6mm rgb(0, 0, 0, 1))"
+document.getElementsByClassName("drops")[0].style.top = "100px"
+document.getElementsByClassName("drops")[0].style.position = "absolute"

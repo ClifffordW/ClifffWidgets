109d507e6a098036e6c7cd7caf27f7e3192b1672
Cliffford W <55302963+ClifffordW@users.noreply.github.com>
2024-02-08 18:54:54 +0100

    Update drop_info.php

--- drop_info.php
+++ drop_info.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-Drops are active! You can associate your twitch.tv account to Klei Account. Simply head on over to https://accounts.klei.com After that just watch the stream for the required time: Current Set - Art Noveau Collection - Icon (30 Minutes) - Portrait (1 Hour 30 Minutes) - The Nouveau Frigid Firepit (3 Hours). Be sure to collect your drop at https://www.twitch.tv/drops/inventory
-=======
--Drops are active! You can associate your twitch.tv account to Klei Account. Simply head on over to https://accounts.klei.com After that just watch the stream for the required time: Current Set - Art Nouveau Collection - Icon (30 Minutes) - Portrait (1 Hour 30 Minutes) - Nouveau Thulecite Suit (3 Hours). Be sure to collect your drop at https://www.twitch.tv/drops/inventory
->>>>>>> parent of 4493271 (Update drop_info.php)
+Drops are currently inactive! You can associate your twitch.tv account to Klei Account. Simply head on over to https://accounts.klei.com After that just watch the stream for the required time.

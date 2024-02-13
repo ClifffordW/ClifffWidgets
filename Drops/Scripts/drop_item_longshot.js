2ed1d1102da6467c618ea19b0631b23c59cc299c
Cliffford W <55302963+ClifffordW@users.noreply.github.com>
2024-02-13 19:56:15 +0100

    Update drop_item_longshot.js

--- Drops/Scripts/drop_item_longshot.js
+++ Drops/Scripts/drop_item_longshot.js
@@ -3,7 +3,7 @@ const DropReleaseDate = new Date("March 24, 2022 18:00:00").getTime();
 const drops = document.getElementsByClassName("drops")[0];
 const body =
     `
-    <link rel="stylesheet" href="Animations/anim_rewritten_test.css">
+    <link rel="stylesheet" href="https://clifffordw.github.io/ClifffWidgets/Drops/Scripts/Animations/anim_rewritten_test.css">
     <img class="bg-flair" src="https://items.kleientertainment.com/images/DST/PROFILEFLAIR_TENT_AN/verylarge">
     <img class="bg-portrait" src="https://items.kleientertainment.com/images/DST/PLAYERPORTRAIT_BG_TENTAN/verylarge">
     <img class="item-frame" src="https://clifffordw.github.io/ClifffWidgets/Drops/Dropcons/Frames/Item/default.png">

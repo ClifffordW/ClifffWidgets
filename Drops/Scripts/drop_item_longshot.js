12601b9c68a695434dfcecf546c9843657cc2ea1
Cliffford W. <55302963+ClifffordW@users.noreply.github.com>
2024-06-27 20:00:24 +0200

    Update drop_item_longshot.js

--- Drops/Scripts/drop_item_longshot.js
+++ Drops/Scripts/drop_item_longshot.js
@@ -4,10 +4,10 @@ const drops = document.getElementsByClassName("drops")[0];
 const body =
     `
     <link rel="stylesheet" href="https://clifffordw.github.io/ClifffWidgets/Drops/Scripts/Animations/anim_rewritten_test.css">
-    <img class="bg-flair" src="https://items.kleientertainment.com/images/DST/PROFILEFLAIR_AMULET_RED_ROSE/verylarge">
-    <img class="bg-portrait" src="https://items.kleientertainment.com/images/DST/PLAYERPORTRAIT_BG_AMULETREDROSE/verylarge">
+    <img class="bg-flair" src="https://items.kleientertainment.com/images/DST/PROFILEFLAIR_EYEBRELLAHAT_ROSE/verylarge">
+    <img class="bg-portrait" src="https://items.kleientertainment.com/images/DST/PLAYERPORTRAIT_BG_EYEBRELLAHATROSE/verylarge">
     <img class="item-frame" src="https://clifffordw.github.io/ClifffWidgets/Drops/Dropcons/Frames/Item/default.png">
-    <img class="bg-item" src="https://items.kleientertainment.com/images/DST/AMULET_RED_ROSE/verylarge">
+    <img class="bg-item" src="https://items.kleientertainment.com/images/DST/EYEBRELLAHAT_ROSE/verylarge">
     <img class="partyhat" src="https://clifffordw.github.io/ClifffWidgets/Drops/Seasonal Icons/winterfeast.png">
     <img class="wip" src="https://i.imgur.com/QqHArTg.png">
     <img class="patreon" src="">

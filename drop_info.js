e16ea72b4926e2107e9b04e3680239b8d8452e45
Cliffford W. <55302963+ClifffordW@users.noreply.github.com>
2024-09-12 22:59:08 +0200

    test

--- drop_info.js
+++ drop_info.js
@@ -1,16 +1,17 @@
-const url = 'https://cdn.forums.klei.com/drops/current_campaign.json';
+const fetch = require('node-fetch');
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    const campaignData = data["Don't Starve Together"] || {};
-    const englishName = campaignData.englishName || 'Unknown Item';
-    const collectionName = campaignData.collectionName || 'Unknown Collection';
+exports.handler = async (event) => {
+  const url = 'https://cdn.forums.klei.com/drops/current_campaign.json';
+  const response = await fetch(url);
+  const data = await response.json();
+  const gameData = data["Don't Starve Together"];
+  const collectionName = gameData.collectionName;
+  const englishName = gameData.englishName;
 
-    const message = `Drops are active! You can associate your twitch.tv account to Klei Account. Simply head on over to https://accounts.klei.com/ After that just watch the stream for the required time: Current Set - ${collectionName} - Icon (30 Minutes) - Portrait (1 Hour 30 Minutes) - ${englishName} (3 Hours). Be sure to collect your drop at https://www.twitch.tv/drops/inventory`;
+  const message = `Drops are active! You can associate your twitch.tv account to Klei Account. Simply head on over to https://accounts.klei.com After that just watch the stream for the required time: Current Set - ${collectionName} - Icon (30 Minutes) - Portrait (1 Hour 30 Minutes) - ${englishName} (3 Hours). Be sure to collect your drop at https://www.twitch.tv/drops/inventory`;
 
-    return message;
-  })
-  .catch(error => {
-    return 'Error fetching the drops information.';
-  });
+  return {
+    statusCode: 200,
+    body: message,
+  };
+};
